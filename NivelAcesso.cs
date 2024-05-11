d31bf6f469340b5abfb94a38a6bb98202bdf8346
giovanacm <141966611+giovanacm@users.noreply.github.com>
2024-05-11 13:33:38 -0300

    Create NivelAcesso.cs
    - Criação Classe Nivel de Acesso

---
+++ NivelAcesso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Definição da classe NivelUsuario
+public class NivelUsuario
+{
+    public string Descricao { get; set; }
+    public List<string> Permissoes { get; set; }
+
+    public NivelUsuario(string descricao, List<string> permissoes)
+    {
+        Descricao = descricao;
+        Permissoes = permissoes;
+    }
+}
