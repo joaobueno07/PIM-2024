268826d8377ff2dbbbd8787cfe77ae801d29cad1
giovanacm <141966611+giovanacm@users.noreply.github.com>
2024-05-12 10:28:39 -0300

    Create Pagamento.cs

---
+++ proj02/Pagamentos/Pagamento.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pagamentos{
+    public class Pagamento{
+        public double Valor { get; set; }
+
+        public bool EfetuarPagemento (notaFiscal dadosNf){
+            
+        }
+    }
+}
